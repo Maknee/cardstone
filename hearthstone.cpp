762fd2653496926e67195db38b7e377ef018a1b1
rahmanma5 <mashour.rahman@drexel.edu>
2017-01-11 12:08:24 -0500

    Update hearthstone.cpp

--- hearthstone.cpp
+++ hearthstone.cpp
@@ -35,10 +35,12 @@ void printBoard(int currentPlayer, vector<Card> collection)
 	
 	for(int i=0;i<collection.length();i++)
 	{
+		int j = 1;
 		if (collection[i].getID == currentPlayer && collection[i].inPlay() == false)
 		{
-			cout << collection[i].getName() << " [(" << collection[i].getCost() << ") " << collection[i].getAttack()
+			cout << j << ": " << collection[i].getName() << " [(" << collection[i].getCost() << ") " << collection[i].getAttack()
 			 << "/" << collection[i].getHealth() << " ]" << endl;
+			 j++;
 		}	
 	}	
 	
@@ -46,13 +48,25 @@ void printBoard(int currentPlayer, vector<Card> collection)
 	
 }
 
+Card draw(int currentPlayer, vector<Card>* collection, vector<Card>* deck)
+{
+	
+}
+
+
 int main()
 {
 	// initialize
 	bool gameOver = false;
 	vector<Card> collection;
+	vector<Card> deck;
+	vector<Card> graveyard;
 	// player 1 draws 3 cards from deck
 	// player 2 draws 4 cards and gets a coin from deck
+	
+	// DECK - another Card vector, draw by searching until finding ID of currentplayer, then swapping with last element,
+	// then returning the Card and pop_back to remove it from deck and make deck smaller.
+	// Card draw(int currentPlayer, vector<Card>* collection, vector<Card>* deck)
 	
 	// collection.push_back(all them cards)
 	
@@ -64,8 +78,8 @@ int main()
 	
 	while(gameOver == false)
 	{
-		// change player turn
-		
+		// change player turn, wipe screen
+		// set summoning sickness to false
 		// if currentPlayer ==1, draw from deck1
 		
 		// draw (add card to large card vector)
@@ -75,6 +89,13 @@ int main()
 		// print board (is inPlay and corresponding playerid)
 		// print hand (is not inPlay and only show own ID)
 		// prompt for action ]
+		
+		/*ACTIONS: ATTACK, 
+		select your attacking minion, and then their minion
+		IF your minion does not have summoning sickness and is in play AND target is in play, proceed or say can't atk
+		to attack: SEARCH FOR CARD WITH MATCHING ID, then with the card thats returned, sample
+		
+		*/
 		
 		// card stuff
 		/*
